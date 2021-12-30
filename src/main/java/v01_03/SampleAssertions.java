339cbf676baff5777e11643fd6ff598c63c5d9f7
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2021-12-30 15:58:37 +0530

    unused files removed

--- src/main/java/v01_03/SampleAssertions.java
+++
@@ -1,7 +0,0 @@
-package v01_03;
-
-public class SampleAssertions {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- src/main/java/v02_04/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package v02_04;
-
-@SuppressWarnings("unused")
-public class HelloWorld {
-    public void printHello() {
-        String hello = "Hello World";
-        System.out.println(hello);
-    }
-}
